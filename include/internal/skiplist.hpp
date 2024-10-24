923f2c6133ea76a0828bd9fd884b8608fc17931e
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-23 23:51:56 -0400

    rewrote insert for skiplist with comments

--- include/internal/skiplist.hpp
+++ include/internal/skiplist.hpp
@@ -7,59 +7,101 @@
 #include <optional>
 #include <random>
 #include <string>
+#include <cstring>
 
 namespace factdb{
 
     template <typename KeyType, typename ValueType>
     struct MemTableEntry {
-        KeyType key;
-        std::optional<ValueType> value;
-        uint64_t timestamp;
-        bool deleted;
+        KeyType key_;
+        std::optional<ValueType> value_;
+        uint64_t timestamp_;
+        bool deleted_;
 
         MemTableEntry(const KeyType& k, const std::optional<ValueType>& v, uint64_t ts, bool del)
-            : key(k), value(v), timestamp(ts), deleted(del) {}
+            : key_(k), value_(v), timestamp_(ts), deleted_(del) {}
     };
 
     template <typename KeyType, typename ValueType>
     struct SkipListNode {
-        MemTableEntry<KeyType, ValueType> entry;
-        std::vector<std::shared_ptr<SkipListNode<KeyType, ValueType>>> forward; // Forward pointers for each level
+        MemTableEntry<KeyType, ValueType> entry_;
+        std::vector<std::shared_ptr<SkipListNode<KeyType, ValueType>>> forward_; // next node at each level
 
         SkipListNode(int level, const MemTableEntry<KeyType, ValueType>& entry)
-            : entry(entry), forward(level, nullptr) {}
+            : entry_(entry), forward_(level, nullptr) {}
     };
-
+    // two directions: forward and down
     template <typename KeyType, typename ValueType>
     class SkipList {
     public:
         SkipList(int max_level, float prob)
-        : max_level(max_level), probability(prob) {
-            head = std::make_shared<SkipListNode<KeyType, ValueType>>(max_level, MemTableEntry<KeyType, ValueType>(KeyType{}, std::nullopt, 0, false));
-            curr_level = 0;
+        : max_level_(max_level), next_lvl_prob_(prob) {
+            head_ = std::make_shared<SkipListNode<KeyType, ValueType>>(max_level, MemTableEntry<KeyType, ValueType>(KeyType{}, std::nullopt, 0, false));
+            highest_lvl_ = 0;
+        }
+        SkipList(int max_level)
+        : max_level_(max_level), next_lvl_prob_(50.0) {
+            head_ = std::make_shared<SkipListNode<KeyType, ValueType>>(max_level, MemTableEntry<KeyType, ValueType>(KeyType{}, std::nullopt, 0, false));
+            highest_lvl_ = 0;
         }
         void insert(KeyType key, ValueType value) {
+            std::shared_ptr<SkipListNode<KeyType, ValueType>> current = head_;
+            std::shared_ptr<SkipListNode<KeyType, ValueType>> to_update[max_level_];
+            memset(to_update, 0, sizeof(std::shared_ptr<SkipListNode<KeyType, ValueType>>) * (max_level_ + 1));
+            
+            //start at highest level of skiplist, move current pointer forward 
+            for(int i = highest_lvl_; i >= 0; i--){ // top level dowmn
+                while(current->forward_[i] != NULL && 
+                        current->forward_[i]->entry_.key_ < key){ // move as far right as possible
+                            current = current->forward_[i];
+                }
+                to_update[i] = current;
+            }
+
+            current = current->forward_[0]; // where we should insert
+
+            if(current == NULL || current->entry_.key_ != key){
+                int r_level = random_level();
+                if(r_level > highest_lvl_){
+                    for(int i = highest_lvl_ + 1; i < r_level + 1; i++){
+                        to_update[i] = head_; //ensure 1st element always pointing to highest as well
+                    }
+                    highest_lvl_ = r_level;
+                }
+
+                std::shared_ptr<SkipListNode<KeyType, ValueType>> new_node = std::make_shared<SkipListNode<KeyType, ValueType>>(
+                                                                max_level_, MemTableEntry<KeyType, ValueType>
+                                                                (KeyType{}, std::nullopt, 0, false));
+                for(int i = 0; i < r_level; i++){
+                    new_node->forward_[i] = to_update[i]->forward_[i];
+                    to_update[i]->forward_[i] = new_node;
+                }
+            }
         }
         bool search(KeyType value){
+
             return false;
         }
         bool update(KeyType key, ValueType value){
+
             return false;
         }
         bool remove(KeyType value){
+
             return false;
         }
         void display() {
             
         }
     private:
-        std::shared_ptr<SkipListNode<KeyType, ValueType>> head;   // Head node of the skiplist
-        int max_level;                                             // Maximum levels in the skiplist
-        int curr_level;                                        // Current top level
-        float probability; 
+        std::shared_ptr<SkipListNode<KeyType, ValueType>> head_;     // head node of the skiplist
+        int max_level_;                                             // maximum levels in the skiplist
+        int highest_lvl_;                                           // current top level
+        float next_lvl_prob_;                                       // maxiumum 
+
         int random_level() {
             int level = 0;
-            while (std::rand() % 2 == 0 && level < max_level) {
+            while (std::rand() % 2 == 0 && level < max_level_) {
                 level++;
             }
             return level;
