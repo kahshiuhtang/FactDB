7c2a2a6764a50649ef110d4205d96a76ee9b1e7d
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-24 23:40:27 -0400

    add in some skiplist tests, still don't pass

---
+++ tests/test_skiplist.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <internal/skiplist.hpp>
+#include <optional>
+
+TEST(SkipListSuite, BasicInsert1) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 3);
+    skipList.insert(4, 3);
+    skipList.display();
+    EXPECT_EQ(skipList.exists(3), true);
+    EXPECT_EQ(skipList.exists(4), true);
+    EXPECT_EQ(skipList.find_value(3), 3);
+    EXPECT_EQ(skipList.find_value(4), 3);
+}
+
+TEST(SkipListSuite, BasicInsert2) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 2);
+    skipList.insert(4, 3);
+    skipList.display();
+    EXPECT_EQ(skipList.find_value(4), 3);
+    EXPECT_EQ(skipList.find_value(3), 2);
+}
+
+TEST(SkipListSuite, BasicSearch1) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 3);
+    skipList.insert(4, 3);
+    skipList.display();
+    EXPECT_EQ(skipList.exists(4), true);
+    EXPECT_EQ(skipList.exists(5), false);
+}
+
+TEST(SkipListSuite, BasicSearch2) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 3);
+    skipList.insert(4, 3);
+    skipList.display();
+    EXPECT_EQ(skipList.exists(4), true);
+    EXPECT_EQ(skipList.exists(3), true);
+}
+
+TEST(SkipListSuite, BasicUpdate1) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 2);
+    skipList.insert(4, 3);
+
+    skipList.update(4, 10);
+    skipList.update(4, 11);
+    EXPECT_EQ(skipList.find_value(4), 11);
+    EXPECT_EQ(skipList.find_value(3), 2);
+}
+
+TEST(SkipListSuite, BasicUpdate2) {
+    factdb::SkipList<int, int> skipList(4, 50.0f);
+    skipList.insert(3, 2);
+    skipList.insert(4, 3);
+
+    skipList.update(4, 10);
+    skipList.update(3, 11);
+    EXPECT_EQ(skipList.find_value(4), 10);
+    EXPECT_EQ(skipList.find_value(3), 11);
+}
