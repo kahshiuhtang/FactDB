aea7d65686de86a93586a0b8091ab926b1635d4c
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-31 23:11:56 -0400

    add memtable update

--- include/memtable/memtable.hpp
+++ include/memtable/memtable.hpp
@@ -6,14 +6,15 @@
 #include <memory>
 
 #include "internal/skiplist.hpp"
+#include "sstable/sstable.hpp"
 
 namespace factdb{
 class Memtable {
 public:
     void insert(std::string partition_key, std::string cluster_key, std::string value);
-    void update();
+    bool update(std::string partition_key, std::string cluster_key, std::string value);
     void remove();
-    bool flush_to_sstable();
+    factdb::SSTable flush_to_sstable();
 private:
     std::unordered_map<std::string, std::shared_ptr<factdb::SkipList<std::string, std::string>>> skiplist_map_; //map<parititon_key, skiplist<cluster_key, value>>
 };
--- src/memtable.cpp
+++ src/memtable.cpp
@@ -7,15 +7,21 @@ void factdb::Memtable::insert(std::string partition_key, std::string cluster_key
         it->second->insert(cluster_key, value);
     } else {
         auto partition_skiplist = std::make_shared<factdb::SkipList<std::string, std::string>>(MAX_SKIPLIST_HEIGHT, NEW_SKIPLIST_LAYER_PROB);
+        partition_skiplist->insert(cluster_key, value);
         skiplist_map_[partition_key] = partition_skiplist; 
     }
 }
-void factdb::Memtable::update(){
-    
+bool factdb::Memtable::update(std::string partition_key, std::string cluster_key, std::string value){
+    auto it = skiplist_map_.find(partition_key);
+    if (it != skiplist_map_.end()) {
+        it->second->update(cluster_key, value);
+        return true;
+    }
+    return false;
 }
 void factdb::Memtable::remove(){
     
 }
-bool factdb::Memtable::flush_to_sstable(){
+factdb::SSTable factdb::Memtable::flush_to_sstable(){
     
 }
