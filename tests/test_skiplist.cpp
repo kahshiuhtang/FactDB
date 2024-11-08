13026ca5c0c763a8a03cbc0953e0c0fe97ca3b38
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-11-07 22:39:51 -0500

    remove comments

--- tests/test_skiplist.cpp
+++ tests/test_skiplist.cpp
@@ -102,24 +102,22 @@ TEST(SkipListIteratorSuite, BasicIteration) {
 TEST(SkipListIteratorSuite, EmptySkipList) {
     factdb::SkipList<int, std::string> skip_list_(4, 50.0f);
     auto it = skip_list_.begin();
-    ASSERT_EQ(it, skip_list_.end());  // Iterator should be equal to end when the skip list is empty
+    ASSERT_EQ(it, skip_list_.end());
 }
 
 TEST(SkipListIteratorSuite, SingleElement) {
     factdb::SkipList<int, std::string> skip_list_(4, 50.0f);
     skip_list_.insert(1, "one");
 
-    // Iterate through the skip list using the iterator
     auto it = skip_list_.begin();
-    ASSERT_NE(it, skip_list_.end());  // Iterator should not be equal to end
-    ASSERT_EQ(it->key_, 1);  // The key should be 1
-    ASSERT_EQ(it->values_.back()->value_, "one");  // The value should be "one"
+    ASSERT_NE(it, skip_list_.end());
+    ASSERT_EQ(it->key_, 1);
+    ASSERT_EQ(it->values_.back()->value_, "one");
 
     ++it;
-    ASSERT_EQ(it, skip_list_.end());  // Iterator should reach end after the first element
+    ASSERT_EQ(it, skip_list_.end());
 }
 
-// Test Case 4: Multiple Elements (Verify correct order of elements)
 TEST(SkipListIteratorSuite, MultipleElements) {
     factdb::SkipList<int, std::string> skip_list_(4, 50.0f);
     skip_list_.insert(1, "one");
@@ -130,8 +128,8 @@ TEST(SkipListIteratorSuite, MultipleElements) {
     int index = 0;
 
     for (auto it = skip_list_.begin(); it != skip_list_.end(); ++it) {
-        ASSERT_EQ(it->key_, index + 1);  // Ensure the key is in the correct order
-        ASSERT_EQ(it->values_.back()->value_, expected_values[index]); // Ensure the value matches
+        ASSERT_EQ(it->key_, index + 1);
+        ASSERT_EQ(it->values_.back()->value_, expected_values[index]);
         index++;
     }
 }
@@ -143,14 +141,13 @@ TEST(SkipListIteratorSuite, PostIncrement) {
 
     auto it = skip_list_.begin();
     
-    auto temp_it = it++; // Get the value before incrementing
+    auto temp_it = it++;
 
-    ASSERT_EQ(temp_it->key_, 1);  // The first element should be 1
-    ASSERT_EQ(temp_it->values_.back()->value_, "one");  // Value should be "one"
+    ASSERT_EQ(temp_it->key_, 1);
+    ASSERT_EQ(temp_it->values_.back()->value_, "one");
 
-    // After post-increment, it should point to the next element
-    ASSERT_EQ(it->key_, 2);  // The second element should be 2
-    ASSERT_EQ(it->values_.back()->value_, "two");  // Value should be "two"
+    ASSERT_EQ(it->key_, 2);
+    ASSERT_EQ(it->values_.back()->value_, "two");
 }
 
 TEST(SkipListIteratorSuite, IteratorEquality) {
@@ -162,8 +159,7 @@ TEST(SkipListIteratorSuite, IteratorEquality) {
     auto it2 = skip_list_.begin();
     auto it3 = skip_list_.end();
 
-    // Test equality
-    ASSERT_EQ(it1, it2);  // Iterator pointing to the same position should be equal
-    ASSERT_NE(it1, it3);  // Iterator at the end should not be equal to the beginning
-    ASSERT_NE(it2, it3);  // Iterator at the end should not be equal to the beginning
+    ASSERT_EQ(it1, it2);
+    ASSERT_NE(it1, it3);
+    ASSERT_NE(it2, it3);
 }
