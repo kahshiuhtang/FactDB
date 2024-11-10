cc9681dbc225cdc2786f0d894bb131f3ce8aeaf3
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-11-10 16:59:57 -0500

    changed to constexpr

--- include/internal/consts.hpp
+++ include/internal/consts.hpp
@@ -1,6 +1,7 @@
 #ifndef CONSTS_FACTDB_HPP
 #define CONSTS_FACTDB_HPP
 
-int MAX_SKIPLIST_HEIGHT = 16;
-float NEW_SKIPLIST_LAYER_PROB = 50.0f;
+constexpr int MAX_SKIPLIST_HEIGHT = 16;
+constexpr float NEW_SKIPLIST_LAYER_PROB = 0.5f;
+
 #endif
